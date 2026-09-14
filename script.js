0e9749e6d946ad00f577b69b8ddd38637f57425e
gomadofu1729 <haruto191219@gmail.com>
2026-09-14 14:39:48 +0900

    なにしたっけ

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ history_file.addEventListener("change", ()=>{
     }
     const 読取= new FileReader();
     読取.addEventListener("load", ()=>{
-        alert(`${history_file.value.name}を読み込んだよ！`);
-        const 送信データ= JSON.parse(読取.result);
-        console.log(送信データ.name);
+        alert(`${history_file.files[0].name}を読み込んだよ！`);
+        const 取得データ= JSON.parse(読取.result);
+        console.log(取得データ.name);
     });
     読取.readAsText(history_file.files[0]);
 });
