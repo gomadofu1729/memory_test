25baa081b02176a9493f087b11ea4ec235e98249
gomadofu1729 <haruto191219@gmail.com>
2026-09-14 12:22:50 +0900

    table解除

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const data_import= document.getElementById("data-import");
 const history_import= document.getElementById("history-import");
-const history_file= document.getElementById("history-file");
 const select_sets= document.getElementById("select-sets");
 const exportBTN= document.getElementById("export");
 const setting= document.getElementById("setting");
 const start= document.getElementById("start");
 
 data_import.addEventListener("click", ()=>{
-    alert("データをインポートするよ！");
+    alert("データインポートの画面を開くよ！");
+    window.location.href= "import.html";
 });
 history_import.addEventListener("click", ()=>{
-    alert("履歴をインポートするよ！");
+    alert(" をインポートするよ！");
+
 })
 exportBTN.addEventListener("click", ()=>{
-    alert(select_sets.value +" をエクスポートするよ！");
+    alert(`${select_sets.value} をエクスポートするよ！`);
+    
 })
 setting.addEventListener("click", ()=>{
-    alert(select_sets.value +" の設定をするよ！");
+    alert(`${select_sets.value} の設定をするよ！`);
+    window.location.href= `setting.html?Set=${select_sets.value}`;
 })
 start.addEventListener("click", ()=>{
-    alert(select_sets.value +" ではじめるよ！");
+    alert(`${select_sets.value} ではじめるよ！`);
+    window.location.href= `memorize.html?Set=${select_sets.value}`;
 })
