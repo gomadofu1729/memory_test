53843764493aed6c10a425aad7f5ba47078c76ce
gomadofu1729 <haruto191219@gmail.com>
2026-09-15 23:53:54 +0900

    id弄るなら今の内

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const data_import= document.getElementById("data-import");
-const history_import= document.getElementById("history-import");
-const history_file= document.getElementById("history-file");
+const cards_import= document.getElementById("cards-import");
+const set_import= document.getElementById("set-import");
+const set_file= document.getElementById("set-file");
 const select_sets= document.getElementById("select-sets");
-const history_export= document.getElementById("history-export");
-const data_export= document.getElementById("data-export");
+const set_export= document.getElementById("set-export");
+const cards_export= document.getElementById("cards-export");
 const setting= document.getElementById("setting");
 const start= document.getElementById("start");
 
@@ -14,35 +14,35 @@ DBリクエスト.addEventListener("upgradeneeded", ()=>{
     const データベース= DBリクエスト.result;
     データベース.createObjectStore("Card_Sets",{keyPath:"id"});
 })
-data_import.addEventListener("click", ()=>{
+cards_import.addEventListener("click", ()=>{
     alert("データインポートの画面を開くよ！");
     window.location.href= "import.html";
 });
-history_import.addEventListener("click", ()=>{
+set_import.addEventListener("click", ()=>{
     alert("ファイルをえらんでね！");
-    history_file.click();
+    set_file.click();
 });
-history_file.addEventListener("change", ()=>{
-    if (!history_file.value){
+set_file.addEventListener("change", ()=>{
+    if (!set_file.value){
         alert("もう一度えらんでね！");
         return;
     }
     const 読取= new FileReader();
     読取.addEventListener("load", ()=>{
-        alert(`${history_file.files[0].name}を読み込んだよ！`);
+        alert(`${set_file.files[0].name}を読み込んだよ！`);
         カードセット= JSON.parse(読取.result);
         const 新選択肢= document.createElement("option");
         新選択肢.value= カードセット.id;
         新選択肢.textContent= カードセット.name;
         select_sets.appendChild(新選択肢);
     });
-    読取.readAsText(history_file.files[0]);
+    読取.readAsText(set_file.files[0]);
 });
-history_export.addEventListener("click", ()=>{
+set_export.addEventListener("click", ()=>{
     alert(`${select_sets.value} をエクスポートするよ！`);
     
 });
-data_export.addEventListener("click", ()=>{
+cards_export.addEventListener("click", ()=>{
 
 })
 setting.addEventListener("click", ()=>{
