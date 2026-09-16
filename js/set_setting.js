ce3375f9073aa13eea8626f9d1b6456381468326
gomadofu1729 <haruto191219@gmail.com>
2026-09-16 13:43:13 +0900

    削除

--- js/set_setting.js
+++ js/set_setting.js
@@ -79,8 +79,13 @@ exclusion_edit.addEventListener("click", ()=>{
 set_export.addEventListener("click", ()=>{
     飛脚.history(カードセット);
 });
-set_delete.addEventListener("click", ()=>{
-
+set_delete.addEventListener("click", async ()=>{
+    const 返答= confirm(`本当に「${カードセット.name}」を削除しますか?`);
+    if(!返答){return;}
+    const 返答2= confirm(`ホンマに? 後悔せん?`);
+    if(!返答2){return;}
+    await 倉庫番.delete(セットID);
+    船頭.index();
 });
 index.addEventListener("click", ()=>{
     船頭.index();
