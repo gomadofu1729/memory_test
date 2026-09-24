d56bffef3db0a6401fd9d120ade91f43bcf4667c
ごまどーふ <haruto191219@gmail.com>
2026-09-24 17:59:15 +0900

    やっぱこっち

--- js/cards.js
+++ js/cards.js
@@ -213,7 +213,8 @@ content_confirm.addEventListener("click", ()=>{
     指定カードセル.textContent= content_input.value;
     指定カードセル.classList.remove("original");
     指定カードセル.classList.add("changed");
-    content_name.textContent= `${指定カードセル.dataset.fieldId}: ${content_input.value}`;
+    const 仮フィールド= 仮カードセット.fields.find((ふ) => ふ.id === 指定カードセル.dataset.fieldId);
+    content_name.textContent= `${仮フィールド.name}: ${content_input.value}`;
 });
 content_reset.addEventListener("click", ()=>{
     const カード= カードセット.cards.find((か) => か.id === 指定カードID);
